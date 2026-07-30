aaa6db8ec9936968fc46393cf4db99d0a5140a3b
AlexHunter48 <opiaprosper64@gmail.com>
2026-07-30 19:11:46 +0100

    fix port

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ import express from "express";
 
 import cors from "cors";
 
-import { config } from "./src/config/env.js";
-
 import connectDB from "./src/config/db.js";
 
 import authRoute from "./src/routes/authRoute.js";
@@ -12,6 +10,8 @@ import bankRoute from "./src/routes/bankRoute.js";
 connectDB();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -35,6 +35,6 @@ app.use("/api/bank", bankRoute);
 app.get("/", (req, res) => {
   res.send("Finora API is up and running!");
 });
-app.listen(config.port, (req, res) => {
-  console.log(`Server running on port ${config.port}`);
+app.listen(PORT, (req, res) => {
+  console.log(`Server running on port ${PORT}`);
 });
