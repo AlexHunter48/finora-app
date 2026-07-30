501ff3901106c498834a1939b6c108921c06f93f
AlexHunter48 <opiaprosper64@gmail.com>
2026-07-30 18:58:53 +0100

    fix cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://finora-app-self.vercel.app"],
+    origin: ["https://finora-app-self.vercel.app"],
     credentials: true,
   }),
 );
